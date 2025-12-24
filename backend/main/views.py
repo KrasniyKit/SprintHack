c5b9cc5e675552abca13e6ddf2a89db4fdb706ab
kolykonon <@kononikhinnikolay@gmail.com>
2025-12-25 01:12:34 +0300

    Добавил API вьюшку для получения списка районов

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.parsers import MultiParser, FormParser
+import json
 
-# Create your views here.
+from .models import BaseStation, District
+from .serializers import DistrictSerializer
+
+
+class DistrictViewSet(viewsets.ModelViewSet):
+    queryset = District.objects.all()
+    serializer_class = DistrictSerializer
+    filterset_fields = ['density']
+
