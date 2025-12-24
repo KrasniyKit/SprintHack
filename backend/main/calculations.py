5fffc1f8cb0d5acb53d12a255d0a71c58df07005
kolykonon <@kononikhinnikolay@gmail.com>
2025-12-24 22:19:30 +0300

    + класс расчета минимального кол-ва станций и метод подсчета радиуса из площади

--- backend/main/calculations.py
+++ backend/main/calculations.py
@@ -16,3 +16,16 @@ class CalculationsResult:
     handover_regulated: bool
     stations_quantity: int
 
+class MinimumStationsCalculator:
+    """Класс для расчета минимального количества станций"""
+
+    BUILDING_COEFS = {
+        "Плотная": 1.21,
+        "Средняя": 0.9,
+        "Сельская": 0.47
+    }
+
+    @staticmethod
+    def calculate_radius(area: float) -> float:
+        """Расчет радиуса зоны обслуживания из формулы в ТЗшке"""
+        return (area / 3.14) ** 0.5
